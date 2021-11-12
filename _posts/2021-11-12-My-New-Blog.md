d649675ba960d848ceb588c661c00c26fcfa2677
Dean Robinson <drdataanalysis@gmail.com>
2021-11-12 10:25:40 +0000

    adding first blog post

---
+++ _posts/2021-11-12-My-New-Blog.md
@@ -0,0 +1,12 @@
+---
+title: "Creating my New Blog!"
+categories:
+  - Blog
+tags:
+  - Jekyll
+  - GitHub
+  - Web Design
+layout: single
+---
+
+Blog post about creating this blog
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Creating my New Blog!",
+        "excerpt":"Blog post about creating this blog  ","categories": ["Blog"],
+        "tags": ["Jekyll","GitHub","Web Design"],
+        "url": "/blog/My-New-Blog/",
+        "teaser": null
+      }]
